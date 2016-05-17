239b751071449cb447c1bfd3e1aec8db0c025721
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2016-05-17 08:35:03 +0200

    Cleaned up PacmanFitnessFunction

--- src/dk/itu/gaer/PacmanFitnessFunction.java
+++ src/dk/itu/gaer/PacmanFitnessFunction.java
@@ -22,7 +22,6 @@ package dk.itu.gaer;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
-import java.nio.DoubleBuffer;
 
 import org.apache.log4j.Logger;
 import org.jgap.BulkFitnessFunction;
@@ -34,9 +33,7 @@ import com.anji.integration.ActivatorTranscriber;
 import com.anji.util.Configurable;
 import com.anji.util.Properties;
 import com.anji.util.Randomizer;
-import dk.itu.gaer.Board;
 
-import dk.itu.gaer.Pacman;
 import java.util.HashSet;
 import java.util.logging.Level;
 
@@ -48,167 +45,46 @@ import java.util.logging.Level;
  */
 public class PacmanFitnessFunction implements BulkFitnessFunction, Configurable {
 
-    private final static String TRACK_LENGTH_KEY = "polebalance.track.length";
-
-    private final static String TIMESTEPS_KEY = "polebalance.timesteps";
-
-    private final static String NUM_TRIALS_KEY = "polebalance.trials";
-
-    private final static String ANGLE_THRESHOLD_KEY = "polebalance.angle.threshold";
-
-    private final static String INPUT_VELOCITY_KEY = "polebalance.input.velocities";
-
-    private final static String POLE_1_LENGTH_KEY = "pole.1.length";
-
-    private final static String POLE_2_LENGTH_KEY = "pole.2.length";
-
-    private final static String START_POLE_ANGLE_1_KEY = "polebalance.pole.angle.start.1";
-
-    private final static String START_POLE_ANGLE_2_KEY = "polebalance.pole.angle.start.2";
-
-    private final static String START_POLE_ANGLE_RANDOM_KEY = "polebalance.pole.angle.start.random";
-
-    private final static String PENALIZE_FOR_ENERGY_USE_KEY = "penalize.for.energy.use";
-
-    private final static String PENALIZE_OSCILLATIONS_KEY = "penalize.oscillations";
-
-// Some useful physical model constants.
-    private static final double GRAVITY = -9.8;
-
-    private static final double MASSCART = 1.0;
-
-    private static final double FORCE_MAG = 10.0;
-
-    /**
-     * seconds between state updates
-     */
-    private static final double TIME_DELTA = 0.01;
-
-    private static final double FOURTHIRDS = 4.0 / 3.0;
-
-    private static final double MUP = 0.000002;
-
-    /**
-     * 0.0174532; 2pi/360
-     */
-    private static final double ONE_DEGREE = Math.PI / 180.0;
-
-    /**
-     * 0.1047192
-     */
-    private static final double SIX_DEGREES = Math.PI / 30.0;
-
-    /**
-     * 0.2094384;
-     */
-    private static final double TWELVE_DEGREES = Math.PI / 15.0;
-
-    /**
-     * 0.3141592;
-     */
-    private static final double EIGHTEEN_DEGREES = Math.PI / 10.0;
-
-    /**
-     * 0.4188790;
-     */
-    private static final double TWENTYFOUR_DEGREES = Math.PI / 7.5;
-
-    /**
-     * 0.628329;
-     */
-    private static final double THIRTYSIX_DEGREES = Math.PI / 5.0;
-
-    /**
-     * 0.87266;
-     */
-    private static final double FIFTY_DEGREES = Math.PI / 3.6;
-
-    /**
-     * 1.256637;
-     */
-    private static final double SEVENTYTWO_DEGREES = Math.PI / 2.5;
-
-    private final static double DEFAULT_TRACK_LENGTH = 4.8;
-
-    private double trackLength = DEFAULT_TRACK_LENGTH;
-
-    private double trackLengthHalfed;
+    private final static String TIMESTEPS_KEY = "pacman.timesteps";
+    private final static String NUM_TRIALS_KEY = "pacman.trials";
 
     private final static int DEFAULT_TIMESTEPS = 10000;
-
     private int maxTimesteps = DEFAULT_TIMESTEPS;
 
     private final static int DEFAULT_NUM_TRIALS = 10;
-
     private int numTrials = DEFAULT_NUM_TRIALS;
-
-    private double poleAngleThreshold = THIRTYSIX_DEGREES;
 
     private final static Logger logger = Logger.getLogger(PacmanFitnessFunction.class);
 
     private ActivatorTranscriber factory;
 
-    private boolean doInputVelocities = true;
-
-    private double poleLength1 = 0.5;
-
-    private double poleMass1 = 0.1;
-
-    private double poleLength2 = 0.05;
-
-    private double poleMass2 = 0.01;
-
-    private double startPoleAngle1 = ONE_DEGREE;
-
-    private double startPoleAngle2 = 0;
-
-    private boolean startPoleAngleRandom = false;
-
-    private boolean penalizeEnergyUse = false;
-
-    private boolean penalizeOscillations = false;
-
     private Random rand;
 
     private boolean showGame = false;
 
-    private void setTrackLength(double aTrackLength) {
-        trackLength = aTrackLength;
-        trackLengthHalfed = trackLength / 2;
-    }
-
     /**
+     * @throws java.lang.Exception
      * @see com.anji.util.Configurable#init(com.anji.util.Properties)
      */
+    @Override
     public void init(Properties props) throws Exception {
         try {
             factory = (ActivatorTranscriber) props.singletonObjectProperty(ActivatorTranscriber.class);
-            setTrackLength(props.getDoubleProperty(TRACK_LENGTH_KEY, DEFAULT_TRACK_LENGTH));
             maxTimesteps = props.getIntProperty(TIMESTEPS_KEY, DEFAULT_TIMESTEPS);
             numTrials = props.getIntProperty(NUM_TRIALS_KEY, DEFAULT_NUM_TRIALS);
-            poleAngleThreshold = props.getDoubleProperty(ANGLE_THRESHOLD_KEY, THIRTYSIX_DEGREES);
-            doInputVelocities = props.getBooleanProperty(INPUT_VELOCITY_KEY, true);
-            poleLength1 = (props.getDoubleProperty(POLE_1_LENGTH_KEY, 0.5) / 2);
-            poleMass1 = (poleLength1 / 5);
-            poleLength2 = (props.getDoubleProperty(POLE_2_LENGTH_KEY, 0.05) / 2);
-            poleMass2 = (poleLength2 / 5);
-            startPoleAngle1 = props.getDoubleProperty(START_POLE_ANGLE_1_KEY, ONE_DEGREE);
-            startPoleAngle2 = props.getDoubleProperty(START_POLE_ANGLE_2_KEY, 0);
-            startPoleAngleRandom = props.getBooleanProperty(START_POLE_ANGLE_RANDOM_KEY, false);
-            penalizeEnergyUse = props.getBooleanProperty(PENALIZE_FOR_ENERGY_USE_KEY, false);
-            penalizeOscillations = props.getBooleanProperty(PENALIZE_OSCILLATIONS_KEY, false);
             Randomizer randomizer = (Randomizer) props.singletonObjectProperty(Randomizer.class);
             rand = randomizer.getRand();
         } catch (Exception e) {
-            throw new IllegalArgumentException("invalid properties: " + e.getClass().toString() + ": "
-                    + e.getMessage());
+            throw new IllegalArgumentException("invalid properties: " + e.getClass().toString() + ": " + e.getMessage());
         }
     }
 
     /**
+     * @param genotypes
      * @see org.jgap.BulkFitnessFunction#evaluate(java.util.List)
      * @see IdentifyImageFitnessFunction#evaluate(Chromosome)
      */
+    @Override
     public void evaluate(List genotypes) {
 
         // evaluate each chromosome
@@ -250,7 +126,7 @@ public class PacmanFitnessFunction implements BulkFitnessFunction, Configurable 
         int stuckCounter = 100;
 
         // Run the pole-balancing simulation.
-        int currentTimestep = 0;
+        int currentTimestep;
         for (currentTimestep = 0; currentTimestep < maxTimesteps; currentTimestep++) {
             // Network activation values
             double[] networkInput = getNetworkInput(pacman.b);
